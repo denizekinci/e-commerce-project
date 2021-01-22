20de0a41487f98a0991629da88f0e4d4ceb088c9
Deniz Ekinci <deniz.ekinci.dev@gmail.com>
2021-01-22 15:51:43 +0300

    added pages and configured routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\front\MainController;
+use App\Http\Controllers\front\ProductController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('index', [MainController::class, 'index'])->name('homepage');
+Route::get('/product',[ProductController::class,'index'])->name('product');
+
+Route::view('/category','front.category')->name('category');
+Route::view('/product/{slug}','front.product-detail')->name('product_detail');
+Route::view('/basket','front.basket')->name('basket');
+Route::view('/login','front.auth.login')->name('login');
+Route::view('/register','front.auth.register')->name('register');
+Route::view('/contact','front.contact')->name('contact');
+
