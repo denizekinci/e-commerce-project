dd6ff7125693a75f31d02f599d2779d0e17f20ee
Deniz Ekinci <deniz.ekinci.dev@gmail.com>
2021-01-22 18:07:22 +0300

    added pages and configured routes - completed

---
+++ resources/views/front/layouts/inc/head.blade.php
@@ -0,0 +1,12 @@
+<!-- Bootstrap styles -->
+<link href={{URL::asset('assets/css/bootstrap.css')}} rel="stylesheet"/>
+<!-- Customize styles -->
+<link href={{URL::asset('style.css')}} rel="stylesheet"/>
+<!-- font awesome styles -->
+<link href={{URL::asset('assets/font-awesome/css/font-awesome.css')}} rel="stylesheet">
+<!--[if lt IE 9]>
+<script src={{URL::asset('http://html5shim.googlecode.com/svn/trunk/html5.js')}}></script>
+<![endif]-->
+
+<!-- Favicons -->
+<link rel="shortcut icon" href={{URL::asset('resources/assets/ico/favicon.ico')}}>
--- routes/web.php
+++ routes/web.php
@@ -2,27 +2,17 @@
 
 use App\Http\Controllers\front\MainController;
 use App\Http\Controllers\front\ProductController;
-
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('index', [MainController::class, 'index'])->name('homepage');
+
+Route::get('', [MainController::class, 'index'])->name('homepage');
 Route::get('/product',[ProductController::class,'index'])->name('product');
 
-Route::view('/category','front.category')->name('category');
+
 Route::view('/product/{slug}','front.product-detail')->name('product_detail');
-Route::view('/basket','front.basket')->name('basket');
+Route::view('/cart','front.cart')->name('cart');
 Route::view('/login','front.auth.login')->name('login');
 Route::view('/register','front.auth.register')->name('register');
 Route::view('/contact','front.contact')->name('contact');
-
+Route::view('/about','front.about')->name('about');
