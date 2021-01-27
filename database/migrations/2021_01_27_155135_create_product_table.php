15c0cb57f31e9a789e2d87b879539fbd8c293202
Deniz Ekinci <deniz.ekinci.dev@gmail.com>
2021-01-27 19:46:20 +0300

    -Added models, migrations, seeders -Added screenshots into readme.md file

---
+++ database/migrations/2021_01_27_155135_create_product_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('title',255);
+            $table->string('slug',255);
+            $table->text('content');
+            $table->unsignedInteger('stock');
+            $table->unsignedDouble('price')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
